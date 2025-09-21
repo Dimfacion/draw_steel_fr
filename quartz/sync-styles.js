ace462795493c455b1b198a84135b3fbb2aa0f77
Dimfacion <arangalad@gmail.com>
2025-09-21 15:08:56 +0200

    Changes on styles

--- quartz/sync-styles.js
+++ quartz/sync-styles.js
@@ -1,9 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+import { appendFileSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-// Copier le CSS d'Obsidian vers Quartz
-const source = '../content/.obsidian/snippets/index.css';
-const dest = 'static/styles/custom.css';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-fs.appendFileSync(source, dest);
+// Lire le contenu du fichier source
+const source = join(__dirname, '../content/.obsidian/snippets/index.css');
+const dest = join(__dirname, '../quartz/styles/custom.scss');
+const cssContent = readFileSync(source, 'utf8');
+
+// L'ajouter au fichier destination
+appendFileSync(dest, '\n\n/* CSS synchronisé depuis Obsidian */\n');
+appendFileSync(dest, cssContent);
+
 console.log('Styles synchronisés !');
